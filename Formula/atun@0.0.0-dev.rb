aa295a4bb8e0f530d07bfabc189a24b85709589e
Dmitry Kireev <dmitry@atd.sh>
2024-12-05 13:26:35 +0000

    Brew formula update for atun version 0.0.0-dev

--- Formula/atun@0.0.0-dev.rb
+++ Formula/atun@0.0.0-dev.rb
@@ -10,7 +10,7 @@ class AtunAT000Dev < Formula
   depends_on :macos
 
   url "https://github.com/AutomationD/atun/releases/download/0.0.0-dev/atun_0.0.0-dev_darwin_amd64.tar.gz"
-  sha256 "1e194bb469fa62f3f9a46b13a16725eba3a9bde092e13eb5b1ef93476c749acb"
+  sha256 "490eb778138a5f9ae0d66be2d76b86b83d8db5d349d376b4712ebc4149dec11c"
 
   def install
     bin.install "atun"
@@ -25,6 +25,9 @@ class AtunAT000Dev < Formula
       EOS
     end
   end
+
+  conflicts_with "atun-dev"
+  conflicts_with "atun-dev@0.0.0-dev"
 
   test do
     system "#{bin}/atun --version"
