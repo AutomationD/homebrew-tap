fc4c1289c5a22042c7b76aa4c82ad84db87cdb65
Dmitry Kireev <dmitry@atd.sh>
2025-03-06 11:09:08 +0200

    Brew formula update for atun version v0.5.1

--- Formula/atun@0.5.1.rb
+++ Formula/atun@0.5.1.rb
@@ -10,16 +10,16 @@ class AtunAT051 < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/AutomationD/atun/releases/download/v0.5.1/atun_0.5.1_darwin_amd64.tar.gz"
-      sha256 "edad92e003093e7b209d3e9036651d4847cfa5df43cd0235bccb83a1ffd72536"
+      url "https://github.com/AutomationD/atun/releases/download/v0.5.1/atun-darwin-amd64.tar.gz"
+      sha256 "5c03825f1c5e582c8a1834878d3b06397781c8d04c1879814af8cfb13c9db30b"
 
       def install
         bin.install "atun"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/AutomationD/atun/releases/download/v0.5.1/atun_0.5.1_darwin_arm64.tar.gz"
-      sha256 "b2706708125fedde4378a249d1e82ce5d9ebccd31282f30026d4a2f3b5e5fcfa"
+      url "https://github.com/AutomationD/atun/releases/download/v0.5.1/atun-darwin-arm64.tar.gz"
+      sha256 "5315dbb92d8353344b2510248faf6875bab18a7022070c0ab048b4f67cd8ada5"
 
       def install
         bin.install "atun"
@@ -30,8 +30,8 @@ class AtunAT051 < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/AutomationD/atun/releases/download/v0.5.1/atun_0.5.1_linux_amd64.tar.gz"
-        sha256 "02dcff2eaa1d0095e84cb9b679fe58cb36caa6f394874f4165d85d87b38b0ece"
+        url "https://github.com/AutomationD/atun/releases/download/v0.5.1/atun-linux-amd64.tar.gz"
+        sha256 "d2caba2ce98eb24bb177a0b3e211b93d7e0baa77688440949211a49a42e517c0"
 
         def install
           bin.install "atun"
@@ -40,8 +40,8 @@ class AtunAT051 < Formula
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/AutomationD/atun/releases/download/v0.5.1/atun_0.5.1_linux_arm64.tar.gz"
-        sha256 "0d9c7d61f545c19f6b4177c27df7a12d8d2d49fe7e286b850cbe2d72962d8657"
+        url "https://github.com/AutomationD/atun/releases/download/v0.5.1/atun-linux-arm64.tar.gz"
+        sha256 "70b804b8bfec0e3e103c90d12b350f07fa23cc7fd1ee81cf5ba2ef2b8e1172bb"
 
         def install
           bin.install "atun"
