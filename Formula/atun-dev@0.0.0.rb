44682277322040f8fb5bf7537813f889f26402f5
Dmitry Kireev <dmitry@atd.sh>
2025-04-19 20:31:37 +0300

    Brew formula update for atun version v0.0.0

--- Formula/atun-dev@0.0.0.rb
+++ Formula/atun-dev@0.0.0.rb
@@ -11,7 +11,7 @@ class AtunDevAT000 < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/AutomationD/atun/releases/download/v0.0.0/atun-darwin-amd64.tar.gz"
-      sha256 "a209cfe040617e9440cf47b40d18a81cec911ee6c5b849d6388231e5700ebb12"
+      sha256 "e2c7f0bcd84231455884cea62d393fd161fe814f54ec55081d8371a16398eced"
 
       def install
         bin.install "atun"
@@ -19,7 +19,7 @@ class AtunDevAT000 < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/AutomationD/atun/releases/download/v0.0.0/atun-darwin-arm64.tar.gz"
-      sha256 "b1109643e3d8d031cfdfa0fb201c7287ec09ad350bb325b81d2ba89a926eea3d"
+      sha256 "08b9052d598450481ef9e5e214dd432b843c68c9e23d02d896a89495918bd13f"
 
       def install
         bin.install "atun"
@@ -31,7 +31,7 @@ class AtunDevAT000 < Formula
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/AutomationD/atun/releases/download/v0.0.0/atun-linux-amd64.tar.gz"
-        sha256 "3ac643a264badebafaa947d322b6ee16f132775d471f2285bb9f491f7f357266"
+        sha256 "cad90f378ee028993839ddb1b30e5068bf09f22ab92431c994c9d4f440f0b06f"
 
         def install
           bin.install "atun"
@@ -41,7 +41,7 @@ class AtunDevAT000 < Formula
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/AutomationD/atun/releases/download/v0.0.0/atun-linux-arm64.tar.gz"
-        sha256 "77eafbd45032148ac1a9177f038f229316c61e5b2780467199599d727ea0957d"
+        sha256 "463400a8fc87e205cfea483f7cbdf5c486f7467815e1a05d00d42234df01a98b"
 
         def install
           bin.install "atun"
