c6eb9e610be7a861437f3e1d654452b79c2c0160
Dmitry Kireev <dmitry@atd.sh>
2025-05-16 18:09:04 +0300

    Brew formula update for atun version v0.8.2

--- Formula/atun.rb
+++ Formula/atun.rb
@@ -5,21 +5,21 @@
 class Atun < Formula
   desc "atun.io is a simple SSM/SSH tunnel manager"
   homepage "https://github.com/DimmKirr/atun"
-  version "0.8.1"
+  version "0.8.2"
   license "Apache 2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/DimmKirr/atun/releases/download/v0.8.1/atun-darwin-amd64.tar.gz"
-      sha256 "59e7ad6e38694ea83404a32073193866e59c274c33afff6a3b9f7d93f6aca09e"
+      url "https://github.com/DimmKirr/atun/releases/download/v0.8.2/atun-darwin-amd64.tar.gz"
+      sha256 "b0890ecf5700ddc0e194168d0ffc437642da97d4e352d45ea8ac1990330f2e2b"
 
       def install
         bin.install "atun"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/DimmKirr/atun/releases/download/v0.8.1/atun-darwin-arm64.tar.gz"
-      sha256 "f2652b41b44950660eb4d8891b03aaf9a99d61dd7828f31fd494ad9facbbaedf"
+      url "https://github.com/DimmKirr/atun/releases/download/v0.8.2/atun-darwin-arm64.tar.gz"
+      sha256 "3b73fc0f4c4acdcce561a32624b7db980a324f3bc8ab6c799955bfe1b96d3ea4"
 
       def install
         bin.install "atun"
@@ -29,15 +29,15 @@ class Atun < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/DimmKirr/atun/releases/download/v0.8.1/atun-linux-amd64.tar.gz"
-      sha256 "aca541d6333f581848f808eeb88c5b060afc92018a245810b6cc054896159e6b"
+      url "https://github.com/DimmKirr/atun/releases/download/v0.8.2/atun-linux-amd64.tar.gz"
+      sha256 "5597a083d41c524f5247976a0493a39473de945f23069acfb8237b384f91cb9d"
       def install
         bin.install "atun"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/DimmKirr/atun/releases/download/v0.8.1/atun-linux-arm64.tar.gz"
-      sha256 "f459359305461775556381e33a64883aafb6af8a5d661b4e09a26a36c69158a7"
+      url "https://github.com/DimmKirr/atun/releases/download/v0.8.2/atun-linux-arm64.tar.gz"
+      sha256 "d3b065b2ed477f21ba07569f8720006526e63286c0bb1f38077005e78314b491"
       def install
         bin.install "atun"
       end
